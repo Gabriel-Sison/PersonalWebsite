77a65dec6bf846791e49fd5996d3e0c820b16a89
CodingBAUS574 <gabrielarceo0183@gmail.com>
2023-10-22 00:27:01 -0700

    edits

--- php.php
+++ php.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset = "UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+
 <body>
-
-<h1>My first PHP page</h1>
-
-<?php
-echo "Hello World!";
-?>
-
+Welcome <?php echo $_POST["name"]; ?>, it is nice to meet you<br>
+Your email address is: <?php echo $_POST["email"]; ?>
 </body>
+
 </html>
